21f8273d0e75d962471d27a2787887cbd72f2c4b
Jorge Pires <jlmp2002@gmail.com>
2025-11-25 15:53:45 -0300

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Acesse <a href="https://www.futco.com.br/">futco.com.br</a></h1>;
+  return <h1>Acesse <a href="https://www.futco.com.br/">futco.com.br</a>💚</h1>;
 }
 
 export default Home;
